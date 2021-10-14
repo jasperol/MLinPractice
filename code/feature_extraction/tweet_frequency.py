eeafabe852bd8774b0b5f730e8be04ad0aadc3c4
Jasper <jasperol@hotmail>
2021-10-14 11:48:12 +0200

    Tweet Frequency Feature implemented
    started implementing the Tweet Frequency Feature

--- code/feature_extraction/tweet_frequency.py
+++ code/feature_extraction/tweet_frequency.py
@@ -5,3 +5,15 @@ Created on Wed Oct 13 21:13:59 2021
 @author: Beck
 """
 
+import nltk
+from code.feature_extraction.feature_extractor import FeatureExtractor
+
+class TweetFrequency(FeatureExtractor):
+    
+    
+    def __init__(self, input_column):
+        super().__init__([input_column], "{0}_tweet_frequency".format(input_column))
+    
+    def _set_variables(self, inputs):
+        
+        df = 
--- test/feature_extraction/tweet_frequency_test.py
+++ test/feature_extraction/tweet_frequency_test.py
@@ -5,3 +5,29 @@ Created on Wed Oct 13 21:14:23 2021
 @author: Beck
 """
 
+import unittest
+import pandas as pd
+from code.feature_extraction.tweet_frequency import TweetFrequency
+import nltk
+
+class TweetFrequencyTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.INPUT_COLUMN = "input"
+        self.tweet_frequency = TweetFrequency(self.INPUT_COLUMN)
+        self.df = pd.DataFrame()
+        
+    def test_input_columns(self):
+        self.assertEqual(self.tweet_frequency._input_columns, [self.INPUT_COLUMN])
+
+    def test_names_places(self):
+        input_text = "John"
+        self.df[self.INPUT_COLUMN] = [input_text]
+        expected_value = 1 
+
+        result = self.names_places_feature._get_values(input_text)
+        self.assertEqual(result, expected_value)
+        
+    
+if __name__ == "__main__":
+    unittest.main()
