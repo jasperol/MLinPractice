d0e6caa3cc1fdca19cdb39f7ea9e16d2e8c4511c
jchburmester <64207537+jchburmester@users.noreply.github.com>
2021-10-21 14:22:26 +0200

    Implementation of tree features
    1. weekday feature
    2. hashtag count and bag of word
    3. most common words

---
+++ code/feature_extraction/names_places.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 13 13:17:39 2021
+
+@author: Beck
+"""
+
+""" this feature returns how many NNP (proper nouns/ names or places) a 
+tweet contains """
+
+import nltk
+from code.feature_extraction.feature_extractor import FeatureExtractor
+from nltk.tokenize import RegexpTokenizer
+
+
+class NamesPlacesFeature(FeatureExtractor):
+    
+    
+    def __init__(self, input_column):
+        super().__init__([input_column], "{0}_NNP".format(input_column))
+    
+    def _get_values(self, inputs):
+                
+        nnp_perc = []
+        for tweet in inputs:
+            tokenizer = RegexpTokenizer(r'\w+')
+            words = tokenizer.tokenize(tweet)
+            pos_tagged = nltk.pos_tag(words)
+            counter = 0
+            for i in pos_tagged:
+                for j in i:
+                    if j == "NNP":
+                        counter += 1
+            nnp_perc.append(counter/len(pos_tagged))
+        return nnp_perc 
---
+++ code/feature_extraction/tweet_words_most_common.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Feature that extracts the most common words in all tweets, to be stored in a bag of words.
+It will then go through each tweet and check how many of the top used words have been used.
+
+Created on Thu Oct 21 11:23:21 2021
+
+@author: jch
+"""
+
+import numpy as np
+import nltk
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+from code.feature_extraction.feature_extractor import FeatureExtractor
+
+# class for extracting the most common hashtags
+class HashtagsMostCommon(FeatureExtractor):
+    
+    # constructor
+    def __init__(self, input_column):
+        # access superclass of all features
+        super().__init__([input_column], "{0}_tweet_words_most_common".format(input_column))
+        
+    def _get_values(self, inputs):
+        
+        # function to drop special characters
+        def join_char(word):
+            new = ''.join(char for char in word if char.isalnum())
+            return new
+        
+        # pre-processing
+        tweets_and_labels = inputs[["tweet_tokenized", "label"]]
+        viral_tweets = inputs.loc[(tweets_and_labels.label == True)]
+        tweets_column = viral_tweets["tweet_tokenized"]
+        
+        # concatenate all tweets into one text
+        text = ' '.join(tweet for tweet in tweets_column)
+        
+        # tokenize text
+        token_list = list(text.split(","))
+        
+        # drop special characters
+        tokens = [join_char(token) for token in token_list]
+        
+        # filter for empty strings and remove stopwords
+        tokens_final = list(filter(lambda x: x != "", tokens))
+        tokens_noStopWords = [word for word in tokens_final if word.lower() not in ENGLISH_STOP_WORDS]
+        
+        # filter for 's' & 'll'
+        tokens_fixed = list(filter(lambda x: x != "s", tokens_noStopWords))
+        tokens_clear = list(filter(lambda x: x != "ll", tokens_fixed))
+        
+        # extract most common words
+        freq = nltk.FreqDist(tokens_clear)
+        most_common_words = freq.most_common(50)
+        
+        # check for each tweet how many of the most common words are included
+        tweets = inputs["tweet_tokenized"]
+        counts = []
+
+        for t in tweets:
+            counter = 0
+    
+            words = t.split("'")
+            tokens = [w for w in words if w.isalnum()]
+        
+            for token in tokens:
+                if any(token in mcw for mcw in most_common_words):
+                    counter += 1
+        
+            counts.append(counter)
+        
+        result = np.array(counts)
+        
+        return result
+        
+        
+        
