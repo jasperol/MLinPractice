db0d8bef02e271d8e4a0e7e10679cecd1f40d75f
jchburmester <64207537+jchburmester@users.noreply.github.com>
2021-10-26 10:33:05 +0200

    Fully merged with branch CHRISTIAN

--- code/feature_extraction/extract_features.py
+++ code/feature_extraction/extract_features.py
@@ -14,18 +14,18 @@ import numpy as np
 from code.feature_extraction.character_length import CharacterLength
 from code.feature_extraction.names_places import NamesPlacesFeature
 from code.feature_extraction.sentiment import Sentiment
-#from code.feature_extraction.tweet_frequency import TweetFrequency
-#from code.feature_extraction.day_of_the_week import DayOfTheWeek
-#from code.feature_extraction.hashtags_most_common import HashtagsMostCommon
-#from code.feature_extraction.hashtags_num import HashtagsCounts
-#from code.feature_extraction.words_most_common import WordsMostCommon
+from code.feature_extraction.tweet_frequency import TweetFrequency
+from code.feature_extraction.day_of_the_week import DayOfTheWeek
+from code.feature_extraction.hashtags_most_common import HashtagsMostCommon
+from code.feature_extraction.hashtags_num import HashtagsCounts
+from code.feature_extraction.words_most_common import WordsMostCommon
 from code.feature_extraction.feature_collector import FeatureCollector
-from code.util import COLUMN_TWEET, COLUMN_LABEL, COLUMN_DATE, COLUMN_TAGS, SUFFIX_TOKENIZED
+from code.util import COLUMN_TWEET, COLUMN_LABEL, COLUMN_DATE, COLUMN_TAGS, COLUMN_USERS, SUFFIX_TOKENIZED
 
 
 # setting up CLI
 parser = argparse.ArgumentParser(description = "Feature Extraction")
-parser.add_argument("input_file", help = "path to the input csv file")
+parser.add_argument("input_file", help = "path to       the input csv file")
 parser.add_argument("output_file", help = "path to the output pickle file")
 parser.add_argument("-e", "--export_file", help = "create a pipeline and export to the given location", default = None)
 parser.add_argument("-i", "--import_file", help = "import an existing pipeline from the given location", default = None)
@@ -54,53 +54,41 @@ else:    # need to create FeatureCollector manually
     features = []
     if args.char_length:
         # character length of original tweet (without any changes)
-<<<<<<< HEAD
-
-     #   features.append(CharacterLength(COLUMN_TWEET))
-
+        features.append(CharacterLength(COLUMN_TWEET))
+        
+    if args.weekday:
+        # day of the week of the tweet
+        features.append(DayOfTheWeek(COLUMN_DATE))
+    
+    if args.hashtags_most_common:
+        # most common words
+        features.append(HashtagsMostCommon(COLUMN_TAGS))
+        
+    if args.hashtags_num:
+        # amount of hashtags
+        features.append(HashtagsCounts(COLUMN_TAGS))
+        
+    if args.words_most_common:
+        # most common words in the tweets
+        features.append(WordsMostCommon(SUFFIX_TOKENIZED))
+        
     if args.names_places:
         # amount of names and places per tweet
         features.append(NamesPlacesFeature(COLUMN_TWEET))
+        
     if args.tweet_frequency:
         # how many tweets posted by one person
         features.append(TweetFrequency(COLUMN_USERS))
-=======
-        features.append(CharacterLength(COLUMN_TWEET))     
->>>>>>> 2f4026f1f890ffb0387808b87ed7fef6f3bf9571
+
     if args.sentiment:
         # sentiment score of tweet between -1 to 1
         features.append(Sentiment(COLUMN_TWEET))
-        
         
     # create overall FeatureCollector
     feature_collector = FeatureCollector(features)
     
     # fit it on the given data set (assumed to be training data)
     feature_collector.fit(df)
-
-        
-"""
-    if args.names_places:
-        # amount of names and places per tweet
-        features.append(NamesPlacesFeature(COLUMN_TWEET))
-    if args.tweet_frequency:
-        # how many tweets posted by one person
-        features.append(TweetFrequency(COLUMN_TWEET))
-    if args.weekday:
-        # day of the week of the tweet
-        features.append(DayOfTheWeek(COLUMN_DATE))
-    if args.hashtags_most_common:
-        # most common words
-        features.append(HashtagsMostCommon(COLUMN_TAGS)[0])
-    if args.hashtags_num:
-        # amount of hashtags
-        features.append(HashtagsCounts(COLUMN_TAGS))
-    if args.words_most_common:
-        # most common words in the tweets
-        features.append(WordsMostCommon(SUFFIX_TOKENIZED)[0])
-       
-"""
-
 
 # apply the given FeatureCollector on the current data set
 # maps the pandas DataFrame to an numpy array
@@ -113,6 +101,7 @@ label_array = label_array.reshape(-1, 1)
 # store the results
 results = {"features": feature_array, "labels": label_array, 
            "feature_names": feature_collector.get_feature_names()}
+
 with open(args.output_file, 'wb') as f_out:
     pickle.dump(results, f_out)
 
